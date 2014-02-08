0372b389cf5cf64f725f90038d70f326252e82b0
Declan Conlon <dec@cantab.net>
2014-02-08 12:00:12 +1100

    Pair it all back again, avoid the jump directly to PolyN

--- src/main/scala/linearsort.scala
+++ src/main/scala/linearsort.scala
@@ -1,12 +1,12 @@
 package discsort
 import scalaz._, Scalaz._
-import shapeless._
+import shapeless._, poly._
 
 // Bungled attempt at implementing generic discrimiated sort,
 // based on the following paper;
 // www.diku.dk/hjemmesider/ansatte/henglein/papers/henglein2011a.pdf
 
-trait SOrder[A]
+abstract class SOrder[A]
 case class Nat0(i: Int) extends SOrder[Int]
 case class Triv0[A]() extends SOrder[A]
 case class SumL[A,B](t1: SOrder[A], t2: SOrder[B]) extends SOrder[A\/B]
@@ -24,19 +24,42 @@ object SOrder {
    def splitW: Int => (Int,Int) = x => ((x>>16) & 65535,x & 65535)
    val ordChar8 = Map0({x: Char => x.toInt},ordNat8)
 
-//   object sdisc extends Poly1 {
-//
-//      implicit def caseNat0 = at[Nat0]( { case Nat0(i) => sdiscNat(i) } )
-//      implicit def caseSumL[A,B,C] =
-//         at[SumL[A,B]]( { case SumL(t1,t2) => { xs: List[(A\/B,C)] =>
-//            sdisc(t1)(lefts) ++ sdisc(t2)(rights)
-//         } } )
-//
-//   }
-
-   def sdiscNat[B](i: Int): Disc[Int,B] = { xs: List[(Int,B)] =>
-      accumArray[B]( { (xs: List[B], x: B) => x :: xs }, List(), (0,i-1),  xs)
+   def sdisc[A,B](ord: SOrder[A], xs: List[(A,B)]): List[B] = ord match {
+      case n: Nat0 => sdiscNat(n,xs)
+      case SumL(orda,ordb) => sdiscSum(orda,ordb,xs)
+      case ProdL(orda,ordb) => sdiscProd(orda,ordb,xs)
+      case Map0(f,ordb) => sdiscMap(f,ordb,xs)
    }
+
+   def sdiscMap[A,B,C](f: A => B, ordb: SOrder[B], xs: List[(A,C)]): List[C] = {
+      val xs_reassoc = xs map { case (a,c) => (f(a),c) }
+      sdisc(ordb,xs_reassoc)
+   }
+
+   def sdiscProd[A,B,C](orda: SOrder[A], ordb: SOrder[B], xs: List[((A,B),C)]): List[C] = {
+      val xs_reassoc = xs map { case ((a,b),c) => (a,(b,c)) }
+      sdisc(orda,xs_reassoc) |> { sdisc(ordb,_) }
+   }
+   def sdiscSum[A,B,C](orda: SOrder[A], ordb: SOrder[B], xs: List[(A\/B,C)]): List[C] = {
+      val (lefts,rights) =
+         xs.foldLeft((List[(A,C)](),List[(B,C)]())) { (acc,x) =>
+            if(x._1.isLeft) ((((~x._1).toOption.get,x._2)) +: acc._1, acc._2)
+            else (acc._1, ((x._1.toOption.get,x._2)) +: acc._2)
+         }
+      sdisc(orda,lefts.reverse) ++ sdisc(ordb,rights.reverse)
+   }
+
+   val v1 = List[(Int\/Int,Int)]((4.left,4),(3.left,3),(5.right,5),(6.right,6),(1.right,1))
+   val res1: List[Int] = sdisc(SumL(Nat0(255),Nat0(255)),v1)
+
+   val v2 = List[((Int,Int),Int)](((4,5),4),((3,6),3),((3,2),5),((1,10),6),((9,3),1))
+   val res2: List[Int] = sdisc(ProdL(Nat0(255),Nat0(255)),v2)
+
+   val v3 = List[(Char,Int)](('y',5),('h',1),('c',3),('l',1),('t',9))
+   val res3: List[Int] = sdisc(ordChar8,v3)
+
+   def sdiscNat[A](z: Nat0, xs: List[(Int,A)]): List[A] =
+      accumArray[A]( { (xs: List[A], x: A) => x :: xs }, List(), (0,z.i-1),  xs)
 
    // Dirty bucket sort
    def accumArray[B](f: (List[B],B) => List[B],
@@ -48,79 +71,8 @@ object SOrder {
          val idx = arg._1
          val l: List[B] = arr(idx)
          val nl = if(l == null) f(init,arg._2) else f(l,arg._2)
-         println(s"Setting idx[$idx] to $nl")
          arr(idx) = nl
       } )
       arr.toList.filter(_!=null).flatten
    }
-}
-
-//object TestCase {
-//   // Given input of type X return a function of
-//   // type List[(A,B)] => List[B] forall B
-//   // Types X & A are related
-//   object filt extends Poly1 {
-//      implicit def caseOne = at[Int]( i => {
-//         object partialbind extends somefn(i);
-//         partialbind
-//      } )
-//      // ...may have other cases
-//   }
-//
-//   class somefn(j: Int) extends Poly1 {
-//      implicit def caseOne[A] = at[List[(Int,A)]]( xs => xs.filter(_._1 == j).map(_._2) )
-//   }
-//
-//   val t1 = filt(4)
-////   val t2 = filt(5)(List((1,1),(5,5)))
-//}
-//
-//object TestCase2 {
-//   // Given input of type X return a function of
-//   // type List[(A,B)] => List[B] forall B
-//   // Types X & A are related
-//   object filt extends Poly1 {
-//      implicit def caseOne[A] = at[Int]( i => {
-//         xs: List[(Int,A)] => xs.filter(_._1 == i).map(_._2)
-//      } )
-//      // ...may have other cases
-//   }
-//
-////   val t1 = filt[String](4)
-////   val t2 = filt[Int](5)(List((1,1),(5,5)))
-//}
-//
-//object TestCase3 {
-//   // Given input of type X return a function of
-//   // type List[(A,B)] => List[B] forall B
-//   // Types X & A are related
-//   object filt extends Poly1 {
-//      implicit def caseOne = at[Int]( i => {
-//         object somefn { def apply[A]: List[(Int,A)] => List[A] = ( xs => xs.filter(_._1 == i).map(_._2) ) }
-//      } )
-//      // ...may have other cases
-//   }
-//
-//   val t1 = filt(4)
-////   val t2 = filt(5)(List((1,1),(5,5)))
-//}
-
-object TestCase {
-   trait MYtype[A]
-   case class Bob(i: Int) extends MYType[Int]
-   case class Wibble[A,B](t1: MYType[A], t2: MYType[B]) extends MYType[A\/B]
-
-   // MYType => ( List[(A,B)] => List[B] )
-   // So we define myfun for each member of the MYType ADT
-   object myfun extends Poly1 {
-      implicit def caseOne = at[Bob]( i => { ... somefn } )
-      implicit def caseTwo[A,B] = at[Wibble[A,B]]( i => { { xs => myfun(as) ++ myfun(bs) } } )
-   }
-
-   object somefn extends Poly1 {
-      implicit def caseOne[A] = at[List[(Int,A)]]( ... )
-   }
-
-   val t1 = myfun(Bob(42))
-   val t2 = myfun(Wibble(Bob(5),Bob(101)))
 }
