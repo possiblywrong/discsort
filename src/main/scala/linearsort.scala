0acca240f3a17f51b4fa11483e36c5a6db424bbe
Declan Conlon <dec@cantab.net>
2014-02-10 22:59:38 +1100

    Use arrays all the way. no improvement though.

--- src/main/scala/linearsort.scala
+++ src/main/scala/linearsort.scala
@@ -1,5 +1,6 @@
 package discsort
 import scala.collection.mutable.ArrayBuffer
+import scala.reflect.ClassTag
 import scalaz._, Scalaz._
 import util.Random._
 import kadai.cmdopts._, CmdOpts._
@@ -14,7 +15,7 @@ case class TrivOrd[A]() extends DiscOrder[A]
 case class SumOrd[A,B](t1: DiscOrder[A], t2: DiscOrder[B]) extends DiscOrder[Either[A,B]]
 case class ProdOrd[A,B](t1: DiscOrder[A], t2: DiscOrder[B]) extends DiscOrder[(A,B)]
 case class MapOrd[A,B](f: A => B, t2: DiscOrder[B]) extends DiscOrder[A]
-case class ListOrd[A](t: DiscOrder[A]) extends DiscOrder[Vector[A]]
+case class ListOrd[A](t: DiscOrder[A]) extends DiscOrder[Array[A]]
 
 object DiscOrder {
    val ordUnit = TrivOrd[Unit]()
@@ -26,12 +27,17 @@ object DiscOrder {
    val ordChar8 = MapOrd({x: Char => x.toInt},ordNat8)
    val ordString8 = ListOrd(ordChar8)
 
-   def dsort[A,B](ord: DiscOrder[A], xs: Vector[(A,B)]): Vector[Vector[B]] =
+   def dosort[A,B:ClassTag](ord: DiscOrder[A], xs: Vector[(A,B)]): Array[Array[B]] =
+      dsort(ord,xs.toArray)
+
+   def dsort[A,B:ClassTag](ord: DiscOrder[A], xs: Array[(A,B)]): Array[Array[B]] =
       xs match {
-         case Vector() => Vector()
-         case Vector((_,v)) => Vector(Vector(v))
+         case Array() => Array.empty[Array[B]]
+         case Array((_,v)) => val inner = new Array[B](1); inner(0) = v
+                              val outer = new Array[Array[B]](1); outer(0) = inner
+                              outer
          case _ => ord match {
-            case TrivOrd() => Vector(xs map { _._2 })
+            case TrivOrd() => val ret = new Array[Array[B]](1); ret(0) = (xs map { _._2 }); ret
             case n: NatOrd => dsortNat(n,xs)
             case SumOrd(orda,ordb) => dsortSum(orda,ordb,xs)
             case ProdOrd(orda,ordb) => dsortProd(orda,ordb,xs)
@@ -40,37 +46,70 @@ object DiscOrder {
          }
       }
 
-   def stripPartition[A](l: Vector[A]): (Either[Unit,(A,Vector[A])])  =
+   def stripPartition[A](l: Array[A]): (Either[Unit,(A,Array[A])])  =
       l.headOption.cata( { x: A => Right((x,l.tail)) }, { Left(()) } )
 
-   def dsortList[A,B](orda: DiscOrder[A], xs: Vector[(Vector[A],B)]): Vector[Vector[B]] =
+   def dsortList[A,B:ClassTag](orda: DiscOrder[A], xs: Array[(Array[A],B)]): Array[Array[B]] =
       dsort(MapOrd(stripPartition[A], SumOrd(ordUnit,ProdOrd(orda,ListOrd(orda)))), xs)
 
-   def dsortMap[A,B,C](f: A => B, ordb: DiscOrder[B], xs: Vector[(A,C)]): Vector[Vector[C]] =
-      dsort(ordb,xs map { case (a,c) => (f(a),c) })
-
-   def dsortProd[A,B,C](orda: DiscOrder[A], ordb: DiscOrder[B], xs: Vector[((A,B),C)]): Vector[Vector[C]] =
-      dsort(orda,xs map { case ((a,b),c) => (a,(b,c)) }) flatMap { dsort(ordb,_) }
-
-   def dsortSum[A,B,C](orda: DiscOrder[A], ordb: DiscOrder[B], xs: Vector[(Either[A,B],C)]): Vector[Vector[C]] = {
-      val (lefts,rights) = xs.foldLeft((Vector[(A,C)](),Vector[(B,C)]())) {
-         case ((ls,rs),(Left(x),v)) => (ls :+ (x,v),rs)
-         case ((ls,rs),(Right(x),v)) => (ls,rs :+ (x,v))
+   def dsortMap[A,B,C:ClassTag](f: A => B, ordb: DiscOrder[B], xs: Array[(A,C)]): Array[Array[C]] = {
+      var i = 0; val sz = xs.size
+      val arr = new Array[(B,C)](sz)
+      while ( i < sz ) {
+         val (a,v) = xs(i)
+         arr(i) = ((f(a),v))
+         i += 1
       }
-      dsort(orda,lefts) ++ dsort(ordb,rights)
+      dsort(ordb,arr)
+   }
+
+   def dsortProd[A,B,C:ClassTag](orda: DiscOrder[A], ordb: DiscOrder[B], xs: Array[((A,B),C)]): Array[Array[C]] = {
+      val arr = ArrayBuffer[(A,(B,C))]()
+      var i = 0; val sz = xs.size
+      while ( i < sz ) {
+         arr += ( xs(i) match { case ((a,b),c) => (a,(b,c)) } )
+         i += 1
+      }
+      val ds1 = dsort(orda,arr.toArray)
+      val ret = ArrayBuffer[Array[C]]()
+      i = 0; val sz2 = ds1.size
+      while ( i < sz2 ) {
+         val ls: Array[Array[C]] = dsort(ordb,ds1(i))
+         var j = 0; val sz3 = ls.size
+         while( j < sz3 ) {
+            ret += ls(j)
+            j += 1
+         }
+         i += 1
+      }
+      ret.toArray
+   }
+
+   def dsortSum[A,B,C:ClassTag](orda: DiscOrder[A], ordb: DiscOrder[B], xs: Array[(Either[A,B],C)]): Array[Array[C]] = {
+      val lefts = ArrayBuffer[(A,C)]()
+      val rights = ArrayBuffer[(B,C)]()
+      var i = 0; val sz = xs.size
+      while ( i < sz ) {
+         xs(i) match {
+            case (Left(x),v) => lefts += ((x,v))
+            case (Right(x),v) => rights += ((x,v))
+         }
+         i += 1
+      }
+      dsort(orda,lefts.toArray) ++ dsort(ordb,rights.toArray)
    }
 
    val v1 = Vector[(Either[Int,Int],Int)]((Left(4),4),(Left(3),3),(Right(5),5),(Right(6),6),(Right(1),1))
-   lazy val resA: Vector[Int] = dsort(SumOrd(NatOrd(255),NatOrd(255)),v1).flatten
+   lazy val resA = dosort(SumOrd(NatOrd(255),NatOrd(255)),v1).flatten
 
    val v2 = Vector[((Int,Int),Int)](((4,5),4),((3,6),3),((3,2),5),((1,10),6),((9,3),1))
-   lazy val resB: Vector[Int] = dsort(ProdOrd(NatOrd(255),NatOrd(255)),v2).flatten
+   lazy val resB = dosort(ProdOrd(NatOrd(255),NatOrd(255)),v2).flatten
 
    val v3 = Vector[(Char,Int)](('y',5),('h',1),('c',3),('l',1),('t',9))
-   lazy val resC: Vector[Int] = dsort(ordChar8,v3).flatten
+   lazy val resC = dosort(ordChar8,v3).flatten
 
    val v4 = Vector[(String,String)](("bb","bb"),("ab","ab"),("aa","aa"),("ba","ba"))
-   lazy val resD: Vector[String] = dsort(ordString8,v4.map { x => (x._1.toVector,x._2) }).flatten
+   lazy val resD = dosort(ordString8,v4.map { x => (x._1.toArray,x._2) }).flatten
 
    val data10 = List.fill(10)(nextInt)
    val data10t = data10.zip(data10)
@@ -81,7 +120,7 @@ object DiscOrder {
    val data1000000 = List.fill(1000000)(nextInt)
    val data1000000t = data1000000.zip(data1000000)
 
-   def dsortNat[A](z: NatOrd, xs: Vector[(Int,A)]): Vector[Vector[A]] = {
+   def dsortNat[A:ClassTag](z: NatOrd, xs: Array[(Int,A)]): Array[Array[A]] = {
       val asize = z.i+1; val input_size = xs.size
       val arr = new Array[ArrayBuffer[A]](asize)
       var i = 0
@@ -91,10 +130,10 @@ object DiscOrder {
                            else arr(idx) += v }
          i += 1
       }
-      var ret = Vector.empty[Vector[A]]
+      var ret = new ArrayBuffer[Array[A]]()
       i = 0
-      while( i < asize ){ if(arr(i) != null) ret = ret :+ arr(i).toVector; i += 1 }
-      ret
+      while( i < asize ){ if(arr(i) != null) ret += arr(i).toArray; i += 1 }
+      ret.toArray
    }
 
    def time[A](f: => A) = {
@@ -120,13 +159,13 @@ object Main extends App {
       val words: Vector[String] = scala.io.Source.fromFile(CArgs.file)
                     .getLines
                     .flatMap(_.split("\\W+")).toVector
-      val wordst: Vector[(Vector[Char],String)] = words.zip(words).map { x => (x._1.toVector,x._2) }
+      val wordst: Vector[(Array[Char],String)] = words.zip(words).map { x => (x._1.toArray,x._2) }
       if(CArgs.cmpsort) while(true) { time{ words.sorted } }
-      else              while(true) { time{ dsort(ordString8,wordst) } }
+      else              while(true) { time{ dosort(ordString8,wordst) } }
    } else {
       val data = Vector.fill(CArgs.size)(nextInt)
       val datat = data.zip(data)
       if(CArgs.cmpsort) while(true) { time{ data.sorted } }
-      else              while(true) { time{ dsort(ordInt32,datat) } }
+      else              while(true) { time{ dosort(ordInt32,datat) } }
    }
 }
