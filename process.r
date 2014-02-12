2e33e20b660f5268f7ad254ab46291d609e25ed0
Declan Conlon <dec@cantab.net>
2014-02-12 15:07:36 +1100

    More observations + min is a better reflection of the asymtotic behaviour

--- process.r
+++ process.r
@@ -1,10 +1,10 @@
 library(ggplot2)
 options(scipen=999)
-ns <- c(100,1000,10000,100000)
+ns <- c(100,1000,10000,50000,100000,200000,300000,400000)
 vals <- data.frame(Time=numeric(0), Sort=character(0), N=numeric(0))
 for( i in ns ){
    print(i)
-   vals <- rbind(vals,data.frame(Time=median(read.csv(paste0("~/tmp/dsort_",i,".data"),header=FALSE)$V1),Sort="discrimination",N=i))
-   vals <- rbind(vals,data.frame(Time=median(read.csv(paste0("~/tmp/dsort_cmp_",i,".data"),header=FALSE)$V1),Sort="compare",N=i))
+   vals <- rbind(vals,data.frame(Time=min(read.csv(paste0("~/tmp/dsort_",i,".data"),header=FALSE)$V1),Sort="discrimination",N=i))
+   vals <- rbind(vals,data.frame(Time=min(read.csv(paste0("~/tmp/dsort_cmp_",i,".data"),header=FALSE)$V1),Sort="compare",N=i))
 }
 ggplot(data=vals, aes(x=N, y=Time, group=Sort, colour=Sort)) + geom_line() + geom_point()
